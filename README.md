314dbed0445f997ff302125573c3b55675d926ed
Zander Dyer <zanderdyer@Zanders-MacBook-Pro.local>
2019-01-24 18:24:39 -0700

    final

--- README.md
+++ README.md
@@ -17,3 +17,26 @@ LIRI will search Spoifty for songs, Bands in Town for concerts, and OMBD for mov
 * Bands In Town API 
 * Spotify API
 * OMDb API
+
+# What Each Command Should Do
+
+* node liri.js concert-this 'artist or band name here' This will search the Bands in Town Artist Events API for an artist and render the information about each event to the terminal.
+* node liri.js spotify-this-song 'song name here' This will search the Spotify API for an artist and render the information about each song to the terminal.
+* node liri.js movie-this 'movie name here' This will search the OMDB API for a movie and render the information about each movie to the terminal.
+* node liri.js 'do-what-it-says' this will give the user a surpise search result!
+
+# Screen Shots 
+
+<img width="564" alt="concert-this no input" src="https://user-images.githubusercontent.com/41213665/51718941-c47d1300-2004-11e9-88cc-9dab3a8c364b.png">
+<img width="567" alt="concert w input" src="https://user-images.githubusercontent.com/41213665/51718931-b5966080-2004-11e9-8978-5a7329a739a7.png">
+<img width="567" alt="spotify" src="https://user-images.githubusercontent.com/41213665/51719038-28074080-2005-11e9-8f39-9a8915df9a7a.png">
+<img width="570" alt="moive no input" src="https://user-images.githubusercontent.com/41213665/51719044-3190a880-2005-11e9-8677-887bca2ad4ab.png">
+<img width="570" alt="movie input" src="https://user-images.githubusercontent.com/41213665/51719046-36edf300-2005-11e9-992e-03acec78453b.png">
+<img width="569" alt="do what it says" src="https://user-images.githubusercontent.com/41213665/51719056-3e150100-2005-11e9-8d0f-f6e10d641499.png">
+
+
+
+
+
+
+
--- liri.js
+++ liri.js
@@ -1,85 +1,113 @@
 //add code to read and set any environment variables with the dotenv package
-require("dotenv").config();
+require('dotenv').config();
 
-//package requirements
-const fs = require('fs');
+//add the code required to import the keys.js file and store it in a variable
+const keys = require('./keys.js');
 const axios = require('axios');
 const moment = require('moment');
 const Spotify = require('node-spotify-api');
-
-//Add the code required to import the keys.js file and store it in a variable
-const keys = require('./keys.js');
+const fs = require(`fs`);
 
 //access your keys information
 const spotify = new Spotify(keys.spotify);
 
-//returns an array containing the command line arguments passed
-let api = process.argv[2];
 
-//user input
-let userIn = () => {
-    let search = [];
-    for (let i = 3; i < process.argv.length; i++) {
-        search += `${process.argv[i]} `
+//returns an array containing the command line arguments passed
+let nodeArgs = process.argv;
+let command = process.argv[2];
+
+//capture user input
+let userInput = '';
+for (let i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+        userInput = `${userInput}+${nodeArgs[i]}`;
     }
-    return search.trim() //trim results
+    else {
+        userInput += nodeArgs[i];
+    }
 }
 
-//output of the data to a log.txt file 
-fs.appendFile("log.txt", `${api},"${userIn()}":`, (err) => {
-    if (err) {
-        throw err
-    }
-    //console.log('Added');
-})
+//switch stmnts
+switch (command) {
+    case 'concert-this':
+        concertSearch(userInput);
+        break;
 
+    case 'spotify-this-song':
+        spotifySearch(userInput);
+        break;
+
+    case 'movie-this':
+        movieSearch(userInput);
+        break;
+
+    case 'do-what-it-says':
+        randomTxt();
+        break;
+
+    default:
+        console.log(`
+    Oops! Please enter a valid command:
+    For concert info, enter 'concert-this' and an artist
+    For movie info, enter 'movie-this' and a movie name
+    For song info, enter 'spotify-this-song' and a song name
+    Or surprise yourself by entering 'do-what-it-says'!
+    `)
+}
 
 //using bands in town api to pull concert info concert-this
-const concert = function () {
-    axios.get(`https://rest.bandsintown.com/artists/${userIn()}/events?app_id=${keys.bands.key}`)
-        .then(function (response) {
-            response.data.forEach(element => {
-                console.log("----------------------------");
-                console.log(`Arena: ${element.venue.name}`);
-                console.log(`Location: ${element.venue.city}, ${element.venue.country}`);
-                console.log(`Time: ${moment(element.datetime).format("MM/DD/YYYY")} \n`);
-            });
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-}
+function concertSearch(userInput) {
+    if (!userInput) {
+        userInput = `Blink 182`;
+        console.log(`Can't think of a band you want to see? Treat yourself to some 90s alt!
+        `);
+    }
 
-//using spotify api to pull song info spofity-this-song
-const song = function (param) {
-    spotify.search(
-        { type: "track", query: `${param}`, limit: 1 },
-        function (err, data) {
-            if (err) {
-                return console.log("Error occurred: " + err);
+    let concertUrl = `https://rest.bandsintown.com/artists/${userInput}/events?app_id=codingbootcamp`;
+
+    axios.get(concertUrl).then(
+        function (response) {
+            if (response.data[0] === undefined) {
+                console.log(`No upcoming events for that artist...try another one!`);
             }
-            console.log('\n---------------')
-            console.log(`\nArtist: ${data.tracks.items[0].artists[0].name}`);
-            console.log(`Track: ${data.tracks.items[0].name}`);
-            console.log(`Album: ${data.tracks.items[0].album.name}`);
-            console.log(`Preview URL: ${data.tracks.items[0].preview_url} \n`);
-            console.log('---------------\n');
-        }
-    );
-}
+            else {
+                console.log(`Artist: ${response.data[0].lineup[0]}`);
+                console.log(`Venue: ${response.data[0].venue.name}`);
+                console.log(`Location: ${response.data[0].venue.city} ${response.data[0].venue.region} ${response.data[0].venue.country}`);
+                let momentDate = moment(response.data[0].datetime).format(`MM DD YYYY`);
+                console.log(`Date: ${momentDate}`);
+                let exportConcert = (`concert-this / Artist: ${response.data[0].lineup[0]} / Venue: ${response.data[0].venue.name} / Location: ${response.data[0].venue.city} ${response.data[0].venue.region} ${response.data[0].venue.country} / Date: ${momentDate},
+            `);
+                fs.appendFile('log.txt', exportConcert, function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log(`
+                    Concert information added to search log!`);
+                    }
+                })
+            }
 
-//axios for OMDB API
-function motionPic(userInput) {
-    if(!userInput) {
-         userInput = `Mr.+Nobody`;
-         console.log(`Can't think of a movie to search for, huh? Here's one you might like!
+        }
+    ).catch(function (error) {
+        console.log(error);
+    });
+} // ends concertSearch function
+
+
+////axios for OMDB API
+function movieSearch(userInput) {
+    if (!userInput) {
+        userInput = `Mr.+Nobody`;
+        console.log(`Can't think of a movie to search for? Here's one you might like!
          `);
     }
 
     let movieUrl = `http://www.omdbapi.com/?t=${userInput}&y=&plot=short&apikey=trilogy`;
 
     axios.get(movieUrl).then(
-        function(response) {
+        function (response) {
             console.log(`Title: ${response.data.Title}`);
             console.log(`Year: ${response.data.Year}`);
             console.log(`IMDB Rating: ${response.data.imdbRating}`);
@@ -90,62 +118,86 @@ function motionPic(userInput) {
             console.log(`Actors: ${response.data.Actors}`);
             let exportMovie = (`movie-this / Title: ${response.data.Title} / Year: ${response.data.Year} / Plot: ${response.data.Plot},
             `)
-            fs.appendFile('log.txt', exportMovie, function(err) {
-                if(err) {
+            fs.appendFile('log.txt', exportMovie, function (err) {
+                if (err) {
                     console.log(err);
                 }
                 else {
                     console.log(`
                     Movie information added to search log!`);
-                    }
-                })
+                }
+            })
         }
-      ).catch(function (error) {
+    ).catch(function (error) {
         console.log(error);
-      });
+    });
 } // ends movieSearch function
-// const motionPic = function () {
-//     if(!userIn){
-//         userIn = `Mr. +Nobody`;
-//         console.log(`Can't think of a movie? Here's a movie you might like!`);
-//     }
-//     let movieUrl = `http://www.omdbapi.com/?t=${userIn}&y=&plot=short&apikey=trilogy`;
-//     axios.get(movieUrl).then(
-//         function (response) {
-//             console.log("\n---------------");
-//             console.log("\n---------------");
-//             console.log(`Title: ${response.data.Title}`);
-//             console.log(`Year: ${response.data.Year}`);
-//             console.log(`imbd: ${response.data.imbdRating}`);
-//             console.log(`Rotten Tomato Rating: ${response.data.Ratings[1].Value}`);
-//             console.log(`Country: ${response.data.Country}`);
-//             console.log(`Language: ${response.data.Language}`);
-//             console.log(`Plot: ${response.data.Plot}`);
-//             console.log(`Actors: ${response.data.Actors}\n`);
 
-//         }
-//     );
-// }
 
-switch (api) {
-    case 'concert-this':
-        concert(userIn())
-        break
-    case 'spotify-this-song':
-        song(userIn())
-        break
-    case 'movie-this':
-        motionPic(userIn())
-        break
-    case 'do-what-it-says':
-        fs.readFile('./random.txt', 'utf8', (error, data) => {
-            if (error) {
-                throw error
+//using spotify api to pull song info spofity-this-song
+function spotifySearch(userInput) {
+    if (!userInput) {
+        userInput = `The Sign Ace of Base`;
+        console.log(`Uh-oh, you didn't input a song name! Try again!
+        `);
+    }
+
+    spotify.search({ type: `track`, query: userInput, limit: 1 })
+        .then(function (response) {
+            //console.log(JSON.stringify(response, null, 2));
+            console.log(`Artist: ${response.tracks.items[0].artists[0].name}`);
+            console.log(`Song Title: ${response.tracks.items[0].name}`);
+            console.log(`Album: ${response.tracks.items[0].album.name}`);
+            let preview;
+            if (response.tracks.items[0].preview_url === undefined || response.tracks.items[0].preview_url === null) {
+                preview = `Sorry...there is no preview available for this song!`;
+            } else {
+                preview = response.tracks.items[0].preview_url;
             }
-            const newSearch = data.split(',');
-            if (newSearch[0] === 'spotify-this-song') {
-                song(newSearch[1])
-            }
+            console.log(`Preview URL: ${preview}`);
+            let exportTrack = (`spotify-this-song / Artist: ${response.tracks.items[0].artists[0].name} / Song Title: ${response.tracks.items[0].name} / Album: ${response.tracks.items[0].album.name},
+               `);
+            fs.appendFile('log.txt', exportTrack, function (err) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log(`
+                    Track information added to search log!`);
+                }
+            })
         })
-        break
-}
+        .catch(function (err) {
+            console.log(err);
+        });
+} // ends spotifySearch function
+
+//output of the data to a log.txt file 
+function randomTxt() {
+    fs.readFile('./random.txt', 'utf8', function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        //console.log(data);
+
+        let dataArr = data.split(',');
+
+        //console.log(dataArr);
+
+        if (dataArr[0] === 'spotify-this-song') {
+            let textSong = dataArr[1];
+            console.log(textSong);
+            spotifySearch(textSong);
+        }
+        else if (dataArr[0] === 'concert-this') {
+            let textArtist = dataArr[1];
+            console.log(textArtist);
+            concertSearch(textArtist);
+        }
+        else if (dataArr[0] === 'movie-this') {
+            let textMovie = dataArr[1];
+            console.log(textMovie);
+            movieSearch(textMovie);
+        }
+    });
+} // ends randomTxt function
